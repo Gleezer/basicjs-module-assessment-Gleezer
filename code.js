bb2082448d25b17e9dc387bf56800aee7bef59e7
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-05-07 20:14:33 +0000

    deploy: c084d41f5bc65cd9afe3a3946f7c2e93ca871d39

--- code.js
+++ code.js
@@ -10,3 +10,44 @@ let endingPages = [4, 9, 13, 17, 19, 20];
 let currentPage = 0;
 
 // Your Code Here.
+// need to start with a promt after the first page is printed
+//need to loop through the pages? or a bunch of if else statements?
+// had to print out pages until an end page is reached
+// once end page is reached print out everything that was selected to the screen
+
+//has to start with page 0, then ask which page to go to
+
+function isEndingPage(currentPage){
+    for(let index = 0; index <= endingPages.length; index++) {
+      if(currentPage === endingPages[index]) {
+        return true;
+      } 
+
+    }
+    return false;
+}
+
+//console.log(isEndingPage(currentPage))
+
+//step 2 while loop
+
+while (currentPage !== null) {
+    currentPage = parseInt(currentPage);
+    document.write(`<p> ${pages[currentPage]} </p>`);
+   
+
+    if(isEndingPage(currentPage) === true){
+        currentPage = null;
+        document.write(`<h2>You Have Reached the End! Game Over! </h2>`);
+    }
+
+    if (currentPage !== null) {
+        currentPage = prompt(pages[currentPage] + "\n What page would you like to go to?");
+        
+        
+        document.write(`<p> You cancelled the story.  </p>`);
+    }
+   
+     
+}
+
